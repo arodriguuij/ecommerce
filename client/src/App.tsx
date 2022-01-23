6bc9035dd6ea5d5500fe0902c2958760de18d9c7
Alejandro Rodriguez <alejandro.rodriguez.escu@gmail.com>
2022-01-23 20:28:07 +0100

    Deploy app to Heroku

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3001/api', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  console.log({ data });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? 'Loading...' : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
